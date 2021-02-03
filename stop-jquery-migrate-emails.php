acb5a3f9a8aff3e8fcc9f1599ac7126688962d18
FreshyMichael <50490309+FreshyMichael@users.noreply.github.com>
2021-02-03 08:19:20 -0500

    Update stop-jquery-migrate-emails.php

--- stop-jquery-migrate-emails.php
+++ stop-jquery-migrate-emails.php
@@ -3,7 +3,7 @@
 * Plugin Name: Stop jQuery Migrate Emails
 * Plugin URI: https://github.com/FreshyMichael/stop-jquery-migrate-emails
 * Description: Say goodbye to Enable jQuery Migrate Helper weekly status email notifications.
-* Version: 1.0.0
+* Version: 1.0.1
 * Author: FreshySites
 * Author URI: https://freshysites.com/
 * License: GNU v3.0
@@ -15,6 +15,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /* Stop jQuery Migrate Emails Start */
 
+function stop_jquery_migrate_emails(){
+
 if ( is_plugin_active( 'enable-jquery-migrate-helper/enable-jquery-migrate-helper.php' ) ) {
     //Enable jQuery Migrate Helper plugin is active
 
@@ -24,6 +26,9 @@ add_action('init', function() {
 }, 100);
 	
 }
+}
+
+add_action('admin_init' , 'stop_jquery_migrate_emails);
 
 /* Stop jQuery Migrate Emails End */
 ?>
